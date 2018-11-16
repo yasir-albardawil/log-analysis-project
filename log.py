d87f6e973f177546c722331265c4f553c740c7a0
Yasir Albardawil <yasir.s.albardawil@gmail.com>
2018-11-16 09:39:04 +0300

    Initial commit

--- log.py
+++ log.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import psycopg2
 
 DATABASE_NAME = 'news'
@@ -12,16 +14,21 @@ def query(sql):
     return results
 
 
+def print_question(question):
+    print(question)
+
+
 def the_most_three_articles():
-    query_the_most_three_articles = '''select articles.title, count(*) as number
+    query = '''select articles.title, count(*) as number
                 from log, articles
                 where log.status='200 OK'
                 and articles.slug = substr(log.path, 10)
                 group by articles.title
                 order by number desc
                 limit 3;'''
-    results = query(query_the_most_three_articles)
-    print("What are the most popular three articles of all time?")
+
+    results = query(query)
+    print_question("What are the most popular three articles of all time?")
 
     # print the result
     for title, number in results:
@@ -29,16 +36,16 @@ def the_most_three_articles():
 
 
 def the_most_popular_article_authors():
-    query_the_most_popular_article_authors = '''select authors.name, count(*) as number
+    query = '''select authors.name, count(*) as number
                 from articles, authors, log
                 where log.status='200 OK'
                 and authors.id = articles.author
                 and articles.slug = substr(log.path, 10)
                 group by authors.name
-                order by number desc;
-                '''
-    results = query(query_the_most_popular_article_authors)
-    print("\nWho are the most popular article authors of all time?")
+                order by number desc;'''
+
+    results = query(query)
+    print_question("\nWho are the most popular article authors of all time?")
 
     # print the result
     for name, number in results:
@@ -46,29 +53,29 @@ def the_most_popular_article_authors():
 
 
 def more_one_percentage_lead_to_error():
-    query_more_one_percentage_lead_to_error = '''WITH all_requests AS (
+    query = '''WITH all_requests AS (
                 SELECT time::date AS day, count(*)
                 FROM log
                 GROUP BY time::date
                 ORDER BY time::date
-              ), error_requests AS (
+                ), error_requests AS (
                 SELECT time::date AS day, count(*)
                 FROM log
                 WHERE status != '200 OK'
                 GROUP BY time::date
                 ORDER BY time::date
-              ), error_rate AS (
+                , error_rate AS (
                 SELECT all_requests.day,
-                  round(((error_requests.count*100.0)/all_requests.count*1.0), 2)
-                  AS percent
+                round(((error_requests.count*100.0)/all_requests.count*1.0), 2)
+                AS percent
                 FROM all_requests, error_requests
                 WHERE all_requests.day = error_requests.day
-              )
-            SELECT * FROM error_rate WHERE percent > 1;  
-                '''
+                )
+                SELECT * FROM error_rate WHERE percent > 1;'''
 
-    results = query(query_more_one_percentage_lead_to_error)
-    print("\nOn which days did more than 1% of requests lead to errors")
+    results = query(query)
+    print_question("\nOn which days did more than"
+                   " 1% of requests lead to errors")
 
     # print the result
     for result in results:
@@ -77,9 +84,10 @@ def more_one_percentage_lead_to_error():
             error_rate=result[1]))
 
 
-# Question #1
-the_most_three_articles()
-# Question #2
-the_most_popular_article_authors()
-# Question #3
-more_one_percentage_lead_to_error()
+if __name__ == '__main__':
+    # Question #1
+    the_most_three_articles()
+    # Question #2
+    the_most_popular_article_authors()
+    # Question #3
+    more_one_percentage_lead_to_error()
